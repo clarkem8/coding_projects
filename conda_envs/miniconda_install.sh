bdb805ecc217bb0301dbddbc03608d96c54e904e
clarkem8 <34151314+clarkem8@users.noreply.github.com>
2020-10-12 22:26:06 +0100

    Create miniconda_install.sh

---
+++ conda_envs/miniconda_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+  
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -P $HOME/tmp/
+chmod 755 $HOME/tmp/Miniconda3-latest-Linux-x86_64.sh
+$HOME/tmp/Miniconda3-latest-Linux-x86_64.sh -b    
