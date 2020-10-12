596db341e849159adc9562a99eecb4960a999ffd
clarkem8 <34151314+clarkem8@users.noreply.github.com>
2020-10-12 22:28:42 +0100

    Update create.sh

--- AMI/create.sh
+++ AMI/create.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo "Hello"
+echo "Running create AMI script"
+
+useradd clarkem8
+s3_bucket="clarkem8-dev-store"
+
+# Copy installs for rlwrap from s3
+aws s3 cp s3://$s3_bucket/rlwrap /usr/bin/
+aws s3 cp s3://$s3_bucket/libtinfo.so.5 /usr/bin/
+
+yum install git
