94f4d3d821a27f0210fe8f13c6357d0387be7e47
clarkem8 <34151314+clarkem8@users.noreply.github.com>
2020-10-12 22:35:04 +0100

    Update create.sh

--- AMI/create.sh
+++ AMI/create.sh
@@ -9,5 +9,5 @@ aws s3 cp s3://$s3_bucket/rlwrap /usr/bin/
 aws s3 cp s3://$s3_bucket/libtinfo.so.5 /usr/bin/
 
 path=echo $(dirname $0)
-chmod 755 $path/
-$path/
+chmod 755 $path/install_packages.sh
+$path/install_packages.sh
