8cb34a729838e49992df351ef1bd5a954f29fff6
clarkem8 <34151314+clarkem8@users.noreply.github.com>
2020-10-24 00:42:53 +0100

    Update create.sh

--- AMI/create.sh
+++ AMI/create.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 echo "Running create AMI script"
 
-useradd clarkem8
-s3_bucket="clarkem8-dev-store"
+# Get Ec2 instance tags
+# GIT_REPO=
+# GIT_USER=
 
+# Add user
+user="clarkem8"
+useradd $user
+
+s3_bucket="clarkem8-dev-store"
 # Copy installs for rlwrap from s3
 aws s3 cp s3://$s3_bucket/rlwrap /usr/bin/
 aws s3 cp s3://$s3_bucket/libtinfo.so.5 /usr/bin/
 
-path=echo $(dirname $0)
-chmod 755 $path/install_packages.sh
-$path/install_packages.sh
+if [[ "$INSTALL_FROM_S3" =~ "True" ]]; then
+    # Download packages from S3 and install
+    # path=echo $(dirname $0)
+    # chmod 755 $path/install_packages.sh
+    # $path/install_packages.sh
+else
+    git clone "https://github.com/$GIT_USER/$GIT_REPO.git" /home/$USER/github/GIT_REPO
+    
+fi
