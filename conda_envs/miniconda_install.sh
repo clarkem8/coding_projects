e47b7a8bfe7c2422fa973bd9b38dc0f78e1c00be
clarkem8 <34151314+clarkem8@users.noreply.github.com>
2020-10-24 00:20:58 +0100

    Update miniconda_install.sh

--- conda_envs/miniconda_install.sh
+++ conda_envs/miniconda_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
   
 # Download miniconda
-wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh -P $HOME/installs/
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -P $HOME/installs/
 chmod 755 $HOME/installs/*
 
 # Install miniconda
 # -p to install into particular directory
 # -b to install in batch and agree to terms
 # -u to update an existing install if it exists
-$HOME/installs/Anaconda3-2019.03-Linux-x86_64.sh -b -u -p $HOME/
+$HOME/installs/Miniconda3-latest-Linux-x86_64.sh -b -u -p $HOME/miniconda
