30ec74bf6c43f0d141edf555fb541990bfd39f02
clarkem8 <34151314+clarkem8@users.noreply.github.com>
2020-11-06 23:42:23 +0000

    Update profile_setup.sh

--- AMI/profile_setup.sh
+++ AMI/profile_setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # Add git bach function to .bash_profile
-echo "git_branch() {" >> $HOME/.bash_profile
+echo "" >> $HOME/.bash_profile
+echo "parse_git_branch() {" >> $HOME/.bash_profile
 echo "  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'" >> $HOME/.bash_profile
 echo "}" >> $HOME/.bash_profile
+export PS1="\u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
+echo "" >> $HOME/.bash_profile
 
 
 INSTANCEID=$(/opt/aws/bin/ec2-metadata |grep instance-id| cut -c 14-80)
