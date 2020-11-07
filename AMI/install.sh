6355f7bd47f46b9e8157cd9e3da7144540e46cf0
clarkem8 <34151314+clarkem8@users.noreply.github.com>
2020-11-07 22:14:00 +0000

    Update install.sh

--- AMI/install.sh
+++ AMI/install.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo $0
+echo "Running $0"
+base_path=echo $(dirname $0)
+
+## Configure aws cli
+# Find aws region from metadata
+REGION=$(/opt/aws/bin/ec2-metadata |grep placement|cut -c 11-20)
+
+mkdir $HOME/.aws
+echo "[default]" >> $HOME/.aws/config
+echo "region=$REGION" >> $HOME/.aws/config
+echo "output=json" >> $HOME/.aws/config
+
+# Source utils script
+source "$base_path/../aws/utils.sh"
+INSTANCE_NAME=echo $(get_ec2_tag Name)
+echo "Instance name is - $INSTANCE_NAME"
