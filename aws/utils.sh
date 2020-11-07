78ab38d9360c94b050ec2e5a43c0179b59551abb
clarkem8 <34151314+clarkem8@users.noreply.github.com>
2020-11-07 00:36:03 +0000

    Update utils.sh

--- aws/utils.sh
+++ aws/utils.sh
@@ -2,10 +2,9 @@
 
 source $HOME/.env_vars
 
-get_ec2_tags () {
+get_ec2_tag () {
     tag_key=$1
     aws ec2 describe-tags \
         --filter "Name=resource-id,Values=$INSTANCEID" | python -c \
             "import sys; import json; print(''.join(x['Value'] for x in json.load(sys.stdin)['Tags'] if x['Key'] == '$tag_key'))"
 }
-
