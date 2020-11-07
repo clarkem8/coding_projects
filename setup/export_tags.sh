010eae8ef99da7b20fef0f8d7fb5613643d103f1
clarkem8 <34151314+clarkem8@users.noreply.github.com>
2020-11-07 23:27:37 +0000

    Update export_tags.sh

--- setup/export_tags.sh
+++ setup/export_tags.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#Source utils script
+# Source utils script
 source ../aws/utils.sh
 
 #Get all tags for EC2 instance
 tags=$(get_all_ec2_tags)
 
-#Echo each tag to env_vars
+# Echo each tag to env_vars
 for t in $tags;do
     tag=$(get_ec2_tag $t)
     echo "$t=$tag" >> $HOME/.env_vars
