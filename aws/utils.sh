10270695658b12bc52e377b5a8aac17beec0f574
Marcus Clarke <clarkem8@tcd.ie>
2020-11-07 00:35:01 +0000

    Adding utils

--- aws/utils.sh
+++ aws/utils.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+source $HOME/.env_vars
+
 get_ec2_tags () {
-    name=$1
-    aws ec2 describe-tags -
-        -filter "Name=resource-id,Values=$INSTANCEID" | python -c 
-            "import sys; import json; print(''.join(x['Value'] for x in json.load(sys.stdin)['Tags'] if x['Key'] == '$name'))"
+    tag_key=$1
+    aws ec2 describe-tags \
+        --filter "Name=resource-id,Values=$INSTANCEID" | python -c \
+            "import sys; import json; print(''.join(x['Value'] for x in json.load(sys.stdin)['Tags'] if x['Key'] == '$tag_key'))"
 }
+
