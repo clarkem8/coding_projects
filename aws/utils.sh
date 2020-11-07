14b7e33b289f5d73fe9bd922a1790c6a0f9db4ad
clarkem8 <34151314+clarkem8@users.noreply.github.com>
2020-11-07 22:30:13 +0000

    Update utils.sh

--- aws/utils.sh
+++ aws/utils.sh
@@ -8,3 +8,9 @@ get_ec2_tag () {
         --filter "Name=resource-id,Values=$INSTANCEID" | python -c \
             "import sys; import json; print(''.join(x['Value'] for x in json.load(sys.stdin)['Tags'] if x['Key'] == '$tag_key'))"
 }
+
+get_all_ec2_tags () {
+    aws ec2 describe-tags \
+        --filter "Name=resource-id,Values=i-04bf78da536b5fda9" | python -c \
+            "import sys; import json; print(' '.join(x['Value'] for x in json.load(sys.stdin)['Tags']))"
+}
