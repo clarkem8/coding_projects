8b97e1f6345b63a39d0639fd845fde0858b197c0
clarkem8 <34151314+clarkem8@users.noreply.github.com>
2020-11-07 23:28:03 +0000

    Update install.sh

--- setup/install.sh
+++ setup/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Running $0"
+base_path=$(dirname $(readlink -f $0))
 
 ## Configure aws cli
 # Find aws region from metadata
@@ -11,5 +12,8 @@ echo "[default]" >> $HOME/.aws/config
 echo "region=$REGION" >> $HOME/.aws/config
 echo "output=json" >> $HOME/.aws/config
 
-# Export TAGS to .env_vars
+# Setup user profile
+$base_path/profile_setup.sh
 
+# Export TAGS to .env_vars
+$base_path/profile_setup.sh
