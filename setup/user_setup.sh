a971e2ab49699d1914b21362904cf4ecbf51d593
clarkem8 <34151314+clarkem8@users.noreply.github.com>
2020-11-08 00:36:49 +0000

    Update user_setup.sh

--- setup/user_setup.sh
+++ setup/user_setup.sh
@@ -2,6 +2,7 @@
 
 echo "Running $0"
 base_path=$(dirname $(readlink -f $0))
+GIT_PATH="$base_path/.."
 
 ## Configure aws cli
 # Find aws region from metadata
@@ -27,3 +28,7 @@ if [[ ! -d $GIT_PATH ]]; then
     echo "Git path is - $GIT_PATH"
     git clone "https://github.com/$GIT_USER/$GIT_REPO.git" $GIT_PATH
 fi
+
+# Install miniconda and initialise
+$base_path/miniconda_install.sh
+/home/$USER/miniconda/bin/conda init
