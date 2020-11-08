a822a889c52bde24152801eae8598f64af0d210e
clarkem8 <34151314+clarkem8@users.noreply.github.com>
2020-11-08 01:15:23 +0000

    Update user_setup.sh

--- setup/user_setup.sh
+++ setup/user_setup.sh
@@ -15,12 +15,12 @@ echo "region=$REGION" >> $HOME/.aws/config
 echo "output=json" >> $HOME/.aws/config
 
 
-# Setup user profile
-$base_path/profile_setup.sh
-
 # Export TAGS to .env_vars
 $base_path/export_tags.sh
 source $HOME/.env_vars
+
+# Setup user profile
+$base_path/profile_setup.sh
 
 # Clone git repo
 GIT_PATH=$HOME/github/$GIT_REPO
