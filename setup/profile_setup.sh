ef631e678a1bf2799cb2ff392a050cb7cdd85a9a
clarkem8 <34151314+clarkem8@users.noreply.github.com>
2020-11-08 01:21:22 +0000

    Update profile_setup.sh

--- setup/profile_setup.sh
+++ setup/profile_setup.sh
@@ -9,7 +9,7 @@ echo "" >> $HOME/.bash_profile
 echo "parse_git_branch() {" >> $HOME/.bash_profile
 echo "  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'" >> $HOME/.bash_profile
 echo "}" >> $HOME/.bash_profile
-echo "export PS1="\u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "" >> $HOME/.bash_profile
+echo 'export PS1="\u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ å"' >> $HOME/.bash_profile
 echo "" >> $HOME/.bash_profile
 
 echo "source $HOME/.env_vars" >> $HOME/.bash_profile
