3b2a5c614fa6817cb586035b0e6c8e6c5a57eb8d
clarkem8 <34151314+clarkem8@users.noreply.github.com>
2020-11-08 00:23:58 +0000

    Update export_tags.sh

--- setup/export_tags.sh
+++ setup/export_tags.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+echo "Running $0"
+base_path=$(dirname $(readlink -f $0))
+
 # Source utils script
-source ../aws/utils.sh
+source $base_path/../aws/utils.sh
 
 #Get all tags for EC2 instance
 tags=$(get_all_ec2_tags)
