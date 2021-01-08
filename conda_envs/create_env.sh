1c4bdf9b3ec626139dfa189aa167055609ac8951
Marcus Clarke <clarkem8@tcd.ie>
2021-01-08 22:27:06 +0000

    Updating conda env create script

--- conda_envs/create_env.sh
+++ conda_envs/create_env.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-env_yml=$1
-echo "Creating conda env from - $env_yml"
-conda env create -f yaml_fies/$env_yml
+echo "Running $0"
+base_path=$(dirname $(readlink -f $0))
+env=$1
+echo "Creating conda env from - $env"
+conda env create -f $base_path/yaml_files/${env}.yml
