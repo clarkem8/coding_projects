0c6889ddfd16952cb2994a5d1d5e20bbcedf4d21
Marcus Clarke <clarkem8@tcd.ie>
2021-01-09 20:45:18 +0000

    Adding pip install to core conda env

--- conda_envs/create_env.sh
+++ conda_envs/create_env.sh
@@ -5,3 +5,8 @@ base_path=$(dirname $(readlink -f $0))
 env=$1
 echo "Creating conda env from - $env"
 conda env create -f $base_path/yaml_files/${env}.yml
+
+if [[ $env == "core" ]]; then
+    "Pip installing coinbase pro package"
+    pip install cbpro
+fi
